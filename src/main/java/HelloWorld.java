fde68cedd1ebac8aa73f09826e12e8ef74e0520f
quwenzhe <quwenzhe66@163.com>
2024-03-21 21:00:41 +0800

    fix: 重命名 class 名称

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,8 @@
-package com.lixiang.cp;
-
 /**
  * @author quwenzhe
  * @date 2024/3/21
  */
-public class Application {
+public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("hello world");
