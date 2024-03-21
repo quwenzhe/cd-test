ef149322e98744b1fdc2d9c3741b1bfae4b2b61b
quwenzhe <quwenzhe66@163.com>
2024-03-21 18:06:14 +0800

    feat: init project

---
+++ src/main/java/com/lixiang/cp/Application.java
@@ -0,0 +1,12 @@
+package com.lixiang.cp;
+
+/**
+ * @author quwenzhe
+ * @date 2024/3/21
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
